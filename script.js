d80de327828a6b3bed61d758ddbb7204baa71841
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-10-17 20:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,4 +15,4 @@ function volume_sphere() {
   document.getElementById("volume").value = volume.toFixed(4);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
