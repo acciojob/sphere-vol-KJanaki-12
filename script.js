84c3e089eb6d2560de9e799e0bac99025f373150
KJanaki-12 <kenchugundujanaki@gmail.com>
2026-07-31 21:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const radius = document.getElementById("radius");
+const volume = document.getElementById("volume");
+
 function volume_sphere() {
     //Write your code here
-  
+	let r = parseInt(radius.value);
+	
+	let v = 1.33*3.42*r*r*r;
+	volume.value = v;
+	// alert(v)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
